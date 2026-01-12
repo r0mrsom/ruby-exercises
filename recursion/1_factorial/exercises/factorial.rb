067212388e9583c5d53faddb7ad385814c5bb908
r0mrsom <173564029+r0mrsom@users.noreply.github.com>
2026-01-12 09:31:16 +0800

    Finish recursion/1_factorial

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,10 @@
 def factorial(num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  if num > 0
+    num * factorial(num - 1)
+  elsif num == 0
+    1
+  else
+    nil
+  end
 end
--- recursion/1_factorial/spec/factorial_spec.rb
+++ recursion/1_factorial/spec/factorial_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe "Factorial" do
       expect(factorial(1)).to eq(1)
     end
 
-    xit "returns 24 when passed the number 4" do
+    it "returns 24 when passed the number 4" do
       expect(factorial(4)).to eq(24)
     end
 
-    xit "returns 3628800 when passed the number 10" do
+    it "returns 3628800 when passed the number 10" do
       expect(factorial(10)).to eq(3628800)
     end
 
-    xit "returns 1 when passed the number 0" do
+    it "returns 1 when passed the number 0" do
       expect(factorial(0)).to eq(1)
     end
 
-    xit "returns nil when passed a negative number" do
+    it "returns nil when passed a negative number" do
       expect(factorial(-5)).to be_nil
     end
   end
